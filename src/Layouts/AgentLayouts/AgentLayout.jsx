43be616e587d5833188988fbc3716fefc74a57ac
basantsaed2 <basantsaed718@gmail.com>
2025-04-07 22:22:28 +0200

    commit

--- src/Layouts/AgentLayouts/AgentLayout.jsx
+++ src/Layouts/AgentLayouts/AgentLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../../Components/AgentComponents/Navbar";
+import Navbar from "../../Components/";
 import SidebarAgent from "../../Components/AgentComponents/SidebarAgent";
 import { Outlet } from "react-router-dom";
 
